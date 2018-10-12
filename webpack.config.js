d922d2f837a444cd9753d56fca6eb69cc24e6f7b
Erik Fisher <efishr4@gmail.com>
2018-10-11 21:05:01 -0700

    webpack sertup

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  fileName: './index.html',
+});
+
+
+module.exports = {
+  entry: './src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test:  /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      }
+    ]
+  },
+  plugins: [htmlPlugin],
+
+};
